3c075116fdd21d83af7147b4d2b6f2c7f776cb11
Tomas Jasek <tomsik68@posteo.net>
2020-09-06 12:19:48 +0200

    Load configuration file

---
+++ src/config.rs
@@ -0,0 +1,64 @@
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::fmt::{self, Debug, Formatter};
+use std::fs::File;
+use std::io::{Read, Result};
+
+#[derive(Deserialize)]
+pub enum PasswordContainer {
+    Plaintext(String),
+    Shell(String),
+}
+
+#[derive(Deserialize, Debug)]
+pub enum Method {
+    StartTls,
+    Tls,
+}
+
+#[derive(Deserialize)]
+pub struct Account {
+    pub host: String,
+    pub port: u16,
+    pub method: Method,
+    pub username: String,
+    pub password: PasswordContainer,
+    pub folder: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub accounts: Vec<Account>,
+}
+
+impl Config {
+    pub fn read_from(path: &str) -> Result<Config> {
+        let mut f = File::open(path)?;
+        debug!("config file open");
+        let mut buf = String::new();
+        f.read_to_string(&mut buf)?;
+        debug!("config file text read");
+        Ok(toml::from_str(&buf)?)
+    }
+}
+
+impl Debug for PasswordContainer {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        use PasswordContainer::*;
+        match &self {
+            Plaintext(_) => write!(f, "[plaintext password]"),
+            Shell(_) => write!(f, "[shell command]"),
+        }
+    }
+}
+
+// we want the Debug impl to not print user details
+impl Debug for Account {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Account {{ method: {:?}, port: {:?} }}",
+            self.method, self.port
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+extern crate env_logger;
+#[macro_use]
+extern crate log;
+extern crate imap;
+extern crate native_tls;
+extern crate serde;
+#[macro_use]
+extern crate quick_error;
+
+mod config;
+mod getmail;
+
+use config::Config;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    // TODO move config elsewhere
+    let config = Config::read_from("config.toml").expect("failed to load config");
+    let handles: Vec<_> = config
+        .accounts
+        .into_iter()
+        .map(|acc| {
+            tokio::spawn(async move {
+                getmail::get(acc).await;
+            })
+        })
+        .collect();
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
 }
